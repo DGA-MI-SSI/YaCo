9a3f6903b05ea0537c709eaa3a9a90f68305c909
Benoît Amiaux <benoit.amiaux@gmail.com>
2017-07-06 16:13:35 +0200

    yaida: remove cruft from auto-generated swig bindings

--- YaLibs/YaToolsPy/YaIDAPy.h
+++ YaLibs/YaToolsPy/YaIDAPy.h
@@ -14,168 +14,58 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-%include "std_pair.i"
-%include "std_string.i"
-%include "std_map.i"
-%include "std_multimap.i"
-%include "std_set.i"
-%include "cstring.i"
-
 %include "std_vector.i"
 %include "stl.i"
 
-%template () std::pair<unsigned long long, int>;
-%template () std::pair<unsigned long long, unsigned long long>;
-%template () std::map<std::pair<unsigned long long, int>,std::string>;
-%template () std::map<std::pair<unsigned long long, unsigned long long>,std::string>;
-%template () std::map<long long,std::string>;
-%template () std::map<int,std::pair<int,int> >;
-%template () std::vector<unsigned char>;
-%template () std::vector<unsigned long long>;
-%template () std::vector<unsigned int>;
 %template () std::vector<ea_t>;
-
-%template () std::map<string,string>;
-
-
 
 %typemap(typecheck) ea_t
 {
-    $1 = (PyLong_Check($input) || PyInt_Check($input))? 1:0;
+    $1 = PyLong_Check($input) || PyInt_Check($input);
 }
 
 %typemap(in) ea_t {
-#ifdef __EA64__
-        //printf("converting from 64b addr\n");
         if (PyLong_Check($input))
-        {
-            $1 = (ea_t)PyLong_AsUnsignedLongLongMask($input);
-        }
+            $1 = static_cast<ea_t>(PyLong_AsUnsignedLongLongMask($input));
         else if(PyInt_Check($input))
-        {
-            $1 = PyInt_AsUnsignedLongMask($input);
-        }
+            $1 = static_cast<ea_t>(PyInt_AsUnsignedLongMask($input));
         else
-        {
-            printf("bad input : 0x%p\n", $input);
-        }
-
-#else
-        //printf("converting from 32b addr\n");
-          if (PyLong_Check($input)) {
-            unsigned long long v = PyLong_AsUnsignedLongLong($input);
-            if (PyErr_Occurred()) {
-            //printf("PyErr_Occurred on long converting 0x%16X", $input);
-            v = 0;
-            } else {
-              $1 = static_cast<ea_t>(v);
-            }
-          } else if (PyInt_Check($input)) {
-          unsigned long long v;
-                v = PyInt_AsLong($input);
-            if (PyErr_Occurred()) {
-                    //printf("PyErr_Occurred on int converting 0x%16X", $input);
-                    v = 0;
-                }
-                else
-                {
-                $1 = static_cast<ea_t>(v);
-                }
-          }
-//      $1 = PyLong_AsUnsignedLongMask($input);
-#endif
+            SWIG_exception_fail(SWIG_ValueError, "invalid ea_t value");
 }
 
 %typemap(directorin) ea_t {
-#ifdef __EA64__
-        //printf("converting from 64b addr\n");
         if (PyLong_Check($input))
-        {
-            $1 = (ea_t)PyLong_AsUnsignedLongLongMask($input);
-        }
+            $1 = static_cast<ea_t>(PyLong_AsUnsignedLongLongMask($input));
         else if(PyInt_Check($input))
-        {
-            $1 = PyInt_AsUnsignedLongMask($input);
-        }
+            $1 = static_cast<ea_t>(PyInt_AsUnsignedLongMask($input));
         else
-        {
-            //printf("bad input : 0x%p\n", $input);
-        }
-
-#else
-        //printf("converting from 32b addr\n");
-          if (PyLong_Check($input)) {
-            unsigned long long v = PyLong_AsUnsignedLongLong($input);
-            if (PyErr_Occurred()) {
-            //printf("PyErr_Occurred on long converting 0x%16X", $input);
-            v = 0;
-            } else {
-              $1 = static_cast<ea_t>(v);
-            }
-          } else if (PyInt_Check($input)) {
-          unsigned long long v;
-                v = PyInt_AsLong($input);
-            if (PyErr_Occurred()) {
-                    //printf("PyErr_Occurred on int converting 0x%16X", $input);
-                    v = 0;
-                }
-                else
-                {
-                $1 = static_cast<ea_t>(v);
-                }
-          }
-//      $1 = PyLong_AsUnsignedLongMask($input);
-#endif
+            SWIG_exception_fail(SWIG_ValueError, "invalid ea_t value");
 }
 
 %typemap(out) ea_t {
 #ifdef __EA64__
-        //printf("converting from 64b addr\n");
-        $result = PyLong_FromUnsignedLongLong((unsigned long long) $1);
+        $result = PyLong_FromUnsignedLongLong($1);
         Py_XINCREF($result);
 #else
-        //printf("converting from 32b addr\n");
-        $result = PyLong_FromUnsignedLong(((unsigned long) ($1 & 0xFFFFFFFF)));
+        $result = PyLong_FromUnsignedLong($1);
         Py_XINCREF($result);
 #endif
 }
-
-%apply ea_t {bmask_t};
 
 %typemap(out)  std::vector< ea_t,std::allocator< ea_t > >
 {
     $result = PyList_New(0);
-    for (auto element : $1)
+    for(const auto element : $1)
     {
 #ifdef __EA64__
-        auto thisobj = PyLong_FromUnsignedLongLong((unsigned long long) element);
+        const auto obj = PyLong_FromUnsignedLongLong(element);
 #else
-        auto thisobj = PyLong_FromUnsignedLong((unsigned long) (element & 0xFFFFFFFF));
+        const auto obj = PyLong_FromUnsignedLong(element);
 #endif
-        Py_XINCREF(thisobj);
-        PyList_Append($result, thisobj);
+        Py_XINCREF(obj);
+        PyList_Append($result, obj);
     }
 }
-
-
-
-//std::vector<std::pair<CommentType_e, std::string>> YaToolsIDANativeLib::get_comments_at_ea(ea_t ea)
-%typemap(out) std::vector<std::pair<enum CommentType_e,std::string>,std::allocator<std::pair<enum CommentType_e,std::string>>>
-//%typemap(out) std::map< std::pair< unsigned long long,CommentType_e >,std::string,std::less< std::pair< unsigned long long,CommentType_e > >,std::allocator< std::pair< std::pair< unsigned long long,CommentType_e > const,std::string > > > const&
-{
-    $result = PyList_New(0);
-//    std::vector<std::pair<enum CommentType_e,std::string>,std::allocator<std::pair<enum CommentType_e,std::string>>> v = $1;
-    for(const auto& it : *&$1)
-    {
-        CommentType_e type = (it).first;
-        const std::string& str = (it).second;
-        auto tuple = PyTuple_New(2);
-        PyTuple_SetItem(tuple, 0, PyInt_FromLong(type));
-        PyTuple_SetItem(tuple, 1, SWIG_From_std_string(str));
-        PyList_Append($result, tuple);
-    }
-}
-
 
 %include <stdint.i>
 %include <std_shared_ptr.i>
@@ -240,9 +130,7 @@
         PyErr_SetString(PyExc_RuntimeError, const_cast<char*>(exc));
         return NULL;
     } catch(const Swig::DirectorException& /*exc*/) {
-//      PyErr_SetString(PyExc_RuntimeError, const_cast<char*>(exc.getMessage()));
         SWIG_fail;
-//      return NULL;
     } catch(const std::exception& exc) {
         PyErr_SetString(PyExc_RuntimeError, const_cast<char*>(exc.what()));
         return NULL;
@@ -255,10 +143,7 @@
     }
  }
 
-
-
 %include "YaToolsIDANativeLib.hpp"
 %include "IDANativeExporter.hpp"
 %include "IDANativeModel.hpp"
 %include "YaToolsHashProvider.hpp"
-
