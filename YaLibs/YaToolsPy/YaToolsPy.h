6abd7a7070da3af6e2c2828a6b6b66efc7589708
Benoît Amiaux <benoit.amiaux@gmail.com>
2017-07-11 14:45:21 +0200

    yapy: merge YaGitPy & YaIDAPy into YaToolsPy

--- YaLibs/YaToolsPy/YaToolsPy.h
+++ YaLibs/YaToolsPy/YaToolsPy.h
@@ -162,6 +162,176 @@ YaToolObjectId YaToolObjectId_From_String(const char* input, size_t input_len);
 %include "YaToolObjectId.hpp"
 %include "Merger.hpp"
 
-// include other yatools python modules
-%include "YaIDAPy.h"
-%include "YaGitPy.h"
+%include "std_vector.i"
+%include "stl.i"
+
+%template () std::vector<ea_t>;
+
+%typemap(typecheck) ea_t
+{
+    $1 = PyLong_Check($input) || PyInt_Check($input);
+}
+
+%typemap(in) ea_t {
+        if (PyLong_Check($input))
+            $1 = static_cast<ea_t>(PyLong_AsUnsignedLongLongMask($input));
+        else if(PyInt_Check($input))
+            $1 = static_cast<ea_t>(PyInt_AsUnsignedLongMask($input));
+        else
+            SWIG_exception_fail(SWIG_ValueError, "invalid ea_t value");
+}
+
+%typemap(directorin) ea_t {
+        if (PyLong_Check($input))
+            $1 = static_cast<ea_t>(PyLong_AsUnsignedLongLongMask($input));
+        else if(PyInt_Check($input))
+            $1 = static_cast<ea_t>(PyInt_AsUnsignedLongMask($input));
+        else
+            SWIG_exception_fail(SWIG_ValueError, "invalid ea_t value");
+}
+
+%typemap(out) ea_t {
+#ifdef __EA64__
+        $result = PyLong_FromUnsignedLongLong($1);
+        Py_XINCREF($result);
+#else
+        $result = PyLong_FromUnsignedLong($1);
+        Py_XINCREF($result);
+#endif
+}
+
+%typemap(out)  std::vector< ea_t,std::allocator< ea_t > >
+{
+    $result = PyList_New(0);
+    for(const auto element : $1)
+    {
+#ifdef __EA64__
+        const auto obj = PyLong_FromUnsignedLongLong(element);
+#else
+        const auto obj = PyLong_FromUnsignedLong(element);
+#endif
+        Py_XINCREF(obj);
+        PyList_Append($result, obj);
+    }
+}
+
+%include <stdint.i>
+%include <std_shared_ptr.i>
+
+%shared_ptr(IExporter)
+
+%{
+//in pro.h, they forbid use of some functions that SWIG needs : use them anyway and f*** ida!
+#define USE_DANGEROUS_FUNCTIONS
+
+#include <memory>
+#include <pro.h>
+#include <kernwin.hpp>
+#include "IDANativeExporter.hpp"
+#include "IDANativeModel.hpp"
+
+#include "YaToolsHashProvider.hpp"
+#include <exception>
+
+#include "Comparable.hpp"
+#include "Hashable.hpp"
+#include "Signature.hpp"
+#include "IModelAccept.hpp"
+#include "IModelVisitor.hpp"
+#include "ExporterValidatorVisitor.hpp"
+#include "MultiplexerDelegatingVisitor.hpp"
+#include "PathDebuggerVisitor.hpp"
+#include "XML/XMLDatabaseModel.hpp"
+#include "XML/XMLExporter.hpp"
+#include "FlatBufferExporter.hpp"
+#include "FlatBufferDatabaseModel.hpp"
+#include "YaToolObjectId.hpp"
+#include "VersionRelation.hpp"
+
+
+%}
+
+%feature("director:except") {
+    if ($error != NULL) {
+//      PyErr_Print();
+//      PyObject *ptype;
+//      PyObject *pvalue;
+//      PyObject *ptraceback;
+//      PyErr_Fetch(&ptype, &pvalue, &ptraceback);
+        //PyObject_CallMethod(ptraceback, const_cast<char*>("print_exc"), NULL);
+        throw Swig::DirectorMethodException();
+    }
+}
+
+%exception {
+    try {
+        $action
+    } catch(const char *exc) {
+        PyErr_SetString(PyExc_RuntimeError, const_cast<char*>(exc));
+        return NULL;
+    } catch(const Swig::DirectorException& /*exc*/) {
+        SWIG_fail;
+    } catch(const std::exception& exc) {
+        PyErr_SetString(PyExc_RuntimeError, const_cast<char*>(exc.what()));
+        return NULL;
+    } catch(const std::string& exc) {
+        PyErr_SetString(PyExc_RuntimeError, const_cast<char*>(exc.c_str()));
+        return NULL;
+    } catch( ... ) {
+        PyErr_SetString(PyExc_RuntimeError, const_cast<char*>("an unexpected error occured (default message)"));
+        return NULL;
+    }
+ }
+
+%include "IDANativeExporter.hpp"
+%include "IDANativeModel.hpp"
+%include "YaToolsHashProvider.hpp"
+
+%include "std_string.i"
+%include "std_map.i"
+%include "std_set.i"
+%include "std_vector.i"
+
+%{
+#include "YaGitLib.hpp"
+#include "ResolveFileConflictCallback.hpp"
+#include <exception>
+%}
+
+namespace std
+{
+	%template (StringSet) std::set<std::string>;
+}
+%template () std::map<string,string>;
+
+%feature("director:except") {
+    if ($error != NULL) {
+        throw Swig::DirectorMethodException();
+    }
+}
+
+%exception {
+	try {
+		$action	
+	} catch(const char *exc) {
+		PyErr_SetString(PyExc_RuntimeError, const_cast<char*>(exc));
+		return NULL;
+	} catch(const Swig::DirectorException& /*exc*/) {
+//		PyErr_SetString(PyExc_RuntimeError, const_cast<char*>(exc.getMessage()));
+ 		SWIG_fail;
+//		return NULL;
+	} catch(const std::exception& exc) {
+		PyErr_SetString(PyExc_RuntimeError, const_cast<char*>(exc.what()));
+		return NULL;
+	} catch(const std::string& exc) {
+		PyErr_SetString(PyExc_RuntimeError, const_cast<char*>(exc.c_str()));
+		return NULL;
+	} catch( ... ) {
+		PyErr_SetString(PyExc_RuntimeError, const_cast<char*>("an unexpected error occured (default message)"));
+		return NULL;
+	}
+}
+%feature("director") ResolveFileConflictCallback;
+
+%include "YaGitLib.hpp"
+%include "ResolveFileConflictCallback.hpp"
