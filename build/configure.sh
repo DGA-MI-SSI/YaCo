36445b55061d057c71505c8de5cc1487f083bc29
Benoît Amiaux <benoit.amiaux@gmail.com>
2017-10-06 09:13:44 -0500

    build: set configure.sh to 64-bit only

--- build/configure.sh
+++ build/configure.sh
@@ -12,32 +12,17 @@ then
 fi
 echo CONFIG=$CONFIG [Debug, Release]
 
-if [ -z "$ARCH" ]
-then
-    ARCH=$(uname -m)
-fi
-echo ARCH=$ARCH [x86, x86_64, x64]
-
-FLAGS=""
-if [ "$ARCH" == "x86_64" ]
-then
-    FLAGS="$FLAGS -m32"
-fi
-if [ "$ARCH" == "x64" ]
-then
-    FLAGS="-fPIC"
-fi
-
 if [ -z "$OUT" ]
 then
-    OUT=../out/${ARCH}_$CONFIG
+    OUT=../out/x64_$CONFIG
 fi
 echo OUT=$OUT
 
-CMAKE_C_FLAGS="${CMAKE_C_FLAGS} $FLAGS -pg"
-CMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} $FLAGS -pg"
+FLAGS="-fPIC -pg"
+C_FLAGS="${CMAKE_C_FLAGS} $FLAGS"
+CXX_FLAGS="${CMAKE_CXX_FLAGS} $FLAGS"
 
 mkdir -p $OUT
 cd $OUT
-cmake ../../build -G "$TARGET" -DCMAKE_BUILD_TYPE="${CONFIG}" -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS}" -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS}"
+cmake ../../build -G "$TARGET" -DCMAKE_BUILD_TYPE="${CONFIG}" -DCMAKE_C_FLAGS="${C_FLAGS}" -DCMAKE_CXX_FLAGS="${CXX_FLAGS}"
 cd ../../build
