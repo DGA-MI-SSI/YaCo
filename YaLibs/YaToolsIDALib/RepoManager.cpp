1deae6fa1cd8b65a1b9aa18e40b0777f410df31e
Maxime Pinard <maxime.pin@live.fr>
2017-10-24 13:01:00 +0200

    yaida: copy_idb_to_local_file bug fix
    Use of get_local_idb_name instead of get_original_idb_name

--- YaLibs/YaToolsIDALib/RepoManager.cpp
+++ YaLibs/YaToolsIDALib/RepoManager.cpp
@@ -311,8 +311,8 @@ void remove_ida_temporary_files(const std::string& idb_path)
 
 std::string copy_idb_to_local_file(const std::string& suffix)
 {
-    std::string orig_file_name{ get_original_idb_name(database_idb, suffix) };
-    save_database_ex(orig_file_name.c_str(), 0);
-    remove_ida_temporary_files(orig_file_name);
-    return orig_file_name;
+    std::string local_file_name{ get_local_idb_name(database_idb, suffix) };
+    save_database_ex(local_file_name.c_str(), 0);
+    remove_ida_temporary_files(local_file_name);
+    return local_file_name;
 }
