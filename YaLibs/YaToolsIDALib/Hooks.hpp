81c5e335b4f7d53dd0b5f3403351f6a901aee3ee
Maxime Pinard <maxime.pin@live.fr>
2017-11-17 16:21:05 +0100

    yaida: switch to native structmember_to_process

--- YaLibs/YaToolsIDALib/Hooks.hpp
+++ YaLibs/YaToolsIDALib/Hooks.hpp
@@ -24,6 +24,8 @@ struct IHooks
 {
     virtual void change_comment(ea_t ea) = 0;
     virtual void update_structure(ea_t struct_id) = 0;
+    virtual void update_structure_member(ea_t struct_id, ea_t member_id, ea_t member_offset) = 0;
+    virtual void delete_structure_member(ea_t struct_id, ea_t member_id, ea_t offset) = 0;
     virtual void update_enum(ea_t enum_id) = 0;
     virtual void add_segment(ea_t start_ea, ea_t end_ea) = 0;
 
