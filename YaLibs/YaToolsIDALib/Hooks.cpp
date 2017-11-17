bd35de64f1a4cef7ce5aee311dd0c7f012fdcd91
Maxime Pinard <maxime.pin@live.fr>
2017-11-17 15:15:56 +0100

    yaida: added save_enums to native

--- YaLibs/YaToolsIDALib/Hooks.cpp
+++ YaLibs/YaToolsIDALib/Hooks.cpp
@@ -18,8 +18,11 @@
 
 #include "Repository.hpp"
 #include "YaToolsHashProvider.hpp"
+#include "IModel.hpp"
+#include "IDANativeModel.hpp"
 
 #include <memory>
+#include <tuple>
 
 namespace
 {
@@ -31,8 +34,18 @@ namespace
 
 
     private:
+        void save_enums(std::shared_ptr<IModelIncremental>& ida_model, IModelVisitor* memory_exporter);
+
         std::shared_ptr<IHashProvider> hash_provider_;
         std::shared_ptr<IRepository> repo_manager_;
+
+        std::set<ea_t> addresses_to_process_;
+        std::set<tid_t> structures_to_process_;
+        std::map<tid_t, std::tuple<tid_t, ea_t>> structmember_to_process_; // map<struct_id, tuple<member_id, offset>>
+        std::set<enum_t> enums_to_process_;
+        std::map<ea_t, tid_t> enummember_to_process_;
+        std::set<ea_t> comments_to_process_;
+        std::set<std::tuple<ea_t, ea_t>> segments_to_process_; // set<tuple<seg_ea_start, seg_ea_end>>
     };
 }
 
@@ -41,6 +54,34 @@ Hooks::Hooks(const std::shared_ptr<IHashProvider>& hash_provider, const std::sha
     , repo_manager_{ repo_manager }
 {
 
+}
+
+void Hooks::save_enums(std::shared_ptr<IModelIncremental>& ida_model, IModelVisitor* memory_exporter)
+{
+    // enums: export modified ones, delete deleted ones
+    for (enum_t enum_id : enums_to_process_)
+    {
+        uval_t enum_idx = get_enum_idx(enum_id);
+        if (enum_idx == BADADDR)
+        {
+            // enum deleted
+            ida_model->delete_enum(*memory_exporter, enum_id);
+            continue;
+        }
+
+        // enum modified
+        ida_model->accept_enum(*memory_exporter, enum_id);
+    }
+
+    // not implemented in Python, TODO after porting to C++ events
+    // enums members : update modified ones, remove deleted ones
+    /*
+    iterate over members :
+        -if the parent enum has been deleted, delete the member
+        -otherwise, detect if the member has been updated or removed
+            -updated : accept enum_member
+            -removed : accept enum_member_deleted
+    */
 }
 
 
