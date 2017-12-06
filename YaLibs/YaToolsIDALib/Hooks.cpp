40b4678ae490a1a5ec1342298f1d2dc96a09c0e2
Maxime Pinard <maxime.pin@live.fr>
2017-12-06 11:38:09 +0100

    yaida: added flow_chart_created event log

--- YaLibs/YaToolsIDALib/Hooks.cpp
+++ YaLibs/YaToolsIDALib/Hooks.cpp
@@ -111,6 +111,7 @@ namespace
         void manage_idasgn_loaded_event(va_list args);
         void manage_kernel_config_loaded_event(va_list args);
         void manage_loader_finished_event(va_list args);
+        void manage_flow_chart_created_event(va_list args);
 
         // Variables
         std::shared_ptr<IHashProvider> hash_provider_;
@@ -153,7 +154,7 @@ static ssize_t idb_event_handler(void* user_data, int notification_code, va_list
         case envent_code::idasgn_loaded:           hooks->manage_idasgn_loaded_event(args); break;
         case envent_code::kernel_config_loaded:    hooks->manage_kernel_config_loaded_event(args); break;
         case envent_code::loader_finished:         hooks->manage_loader_finished_event(args); break;
-        case envent_code::flow_chart_created:      LOG_EVENT("flow_chart_created"); break;
+        case envent_code::flow_chart_created:      hooks->manage_flow_chart_created_event(args); break;
         case envent_code::compiler_changed:        LOG_EVENT("compiler_changed"); break;
         case envent_code::changing_ti:             LOG_EVENT("changing_ti"); break;
         case envent_code::ti_changed:              LOG_EVENT("ti_changed"); break;
@@ -673,6 +674,18 @@ void Hooks::manage_loader_finished_event(va_list args)
     UNUSED(neflags);
     if (LOG_EVENTS)
         LOG_EVENT("External file loader for %s files finished its work", filetypename);
+}
+
+void Hooks::manage_flow_chart_created_event(va_list args)
+{
+    qflow_chart_t* fc = va_arg(args, qflow_chart_t*);
+
+    if (LOG_EVENTS)
+    {
+        qstring buffer;
+        get_func_name(&buffer, fc->pfn->start_ea);
+        LOG_EVENT("Gui has retrieved a function flow chart (from " EA_FMT " to " EA_FMT ", name: %s, function: %s)", fc->bounds.start_ea, fc->bounds.end_ea, fc->title.c_str(), buffer.c_str());
+    }
 }
 
 
