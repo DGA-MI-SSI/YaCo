5d4b6ceee6422c35324cd5949a97b52da1769c76
Fridtjof Mund <2780577+fridtjof@users.noreply.github.com>
2017-12-07 21:17:57 +0100

    update arch in deploy_dev.sh

--- build/deploy_dev.sh
+++ build/deploy_dev.sh
@@ -7,5 +7,5 @@ ln -s $ROOT_DIR/YaCo/yaco_plugin.py
 
 # yatools
 mkdir -p YaTools
-ln -s $ROOT_DIR/bin/yaco_d_x86/YaTools/bin YaTools/
+ln -s $ROOT_DIR/bin/yaco_d_x64/YaTools/bin YaTools/
 ln -s $ROOT_DIR/YaCo YaTools/
