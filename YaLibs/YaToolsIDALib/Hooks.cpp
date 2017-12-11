a7494a5094653dea676609a3498fca4acde6c0cf
Maxime Pinard <maxime.pin@live.fr>
2017-12-11 15:17:33 +0100

    yaida: added renaming_struc_member event log

--- YaLibs/YaToolsIDALib/Hooks.cpp
+++ YaLibs/YaToolsIDALib/Hooks.cpp
@@ -148,6 +148,7 @@ namespace
         void struc_member_created_event(va_list args);
         void deleting_struc_member_event(va_list args);
         void struc_member_deleted_event(va_list args);
+        void renaming_struc_member_event(va_list args);
 
         // Variables
         std::shared_ptr<IHashProvider> hash_provider_;
@@ -225,7 +226,7 @@ namespace
             case envent_code::struc_member_created:    hooks->struc_member_created_event(args); break;
             case envent_code::deleting_struc_member:   hooks->deleting_struc_member_event(args); break;
             case envent_code::struc_member_deleted:    hooks->struc_member_deleted_event(args); break;
-            case envent_code::renaming_struc_member:   LOG_EVENT("renaming_struc_member"); break;
+            case envent_code::renaming_struc_member:   hooks->renaming_struc_member_event(args); break;
             case envent_code::struc_member_renamed:    LOG_EVENT("struc_member_renamed"); break;
             case envent_code::changing_struc_member:   LOG_EVENT("changing_struc_member"); break;
             case envent_code::struc_member_changed:    LOG_EVENT("struc_member_changed"); break;
@@ -1185,6 +1186,22 @@ void Hooks::struc_member_deleted_event(va_list args)
         const auto struc_name = qpool_.acquire();
         get_struc_name(&*struc_name, sptr->id);
         LOG_EVENT("Structure type %s member at offset 0x%" EA_PREFIX "X has been deleted", struc_name->c_str(), offset);
+    }
+}
+
+void Hooks::renaming_struc_member_event(va_list args)
+{
+    struc_t* sptr = va_arg(args, struc_t*);
+    member_t* mptr = va_arg(args, member_t*);
+    const char* newname = va_arg(args, const char*);
+
+    if (LOG_EVENTS)
+    {
+        const auto struc_name = qpool_.acquire();
+        get_struc_name(&*struc_name, sptr->id);
+        const auto member_name = qpool_.acquire();
+        get_member_name(&*member_name, mptr->id);
+        LOG_EVENT("A member of structure type %s is to be renamed from %s to %s", struc_name->c_str(), member_name->c_str(), newname);
     }
 }
 
