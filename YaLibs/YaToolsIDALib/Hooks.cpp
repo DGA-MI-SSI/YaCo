aa0ddba7e6359dc13e087ee269fd636fa0ea74e0
Maxime Pinard <maxime.pin@live.fr>
2017-12-12 15:11:40 +0100

    yaida: added segm_class_changed event log

--- YaLibs/YaToolsIDALib/Hooks.cpp
+++ YaLibs/YaToolsIDALib/Hooks.cpp
@@ -164,6 +164,7 @@ namespace
         void changing_segm_name_event(va_list args);
         void segm_name_changed_event(va_list args);
         void changing_segm_class_event(va_list args);
+        void segm_class_changed_event(va_list args);
 
         // Variables
         std::shared_ptr<IHashProvider> hash_provider_;
@@ -257,7 +258,7 @@ namespace
             case envent_code::changing_segm_name:      hooks->changing_segm_name_event(args); break;
             case envent_code::segm_name_changed:       hooks->segm_name_changed_event(args); break;
             case envent_code::changing_segm_class:     hooks->changing_segm_class_event(args); break;
-            case envent_code::segm_class_changed:      LOG_EVENT("segm_class_changed"); break;
+            case envent_code::segm_class_changed:      hooks->segm_class_changed_event(args); break;
             case envent_code::segm_attrs_updated:      LOG_EVENT("segm_attrs_updated"); break;
             case envent_code::segm_moved:              LOG_EVENT("segm_moved"); break;
             case envent_code::allsegs_moved:           LOG_EVENT("allsegs_moved"); break;
@@ -1612,6 +1613,19 @@ void Hooks::changing_segm_class_event(va_list args)
         const auto segm_class = qpool_.acquire();
         get_segm_class(&*segm_class, s);
         LOG_EVENT("Segment %s class is being changed from %s", segm_name->c_str(), segm_class->c_str());
+    }
+}
+
+void Hooks::segm_class_changed_event(va_list args)
+{
+    segment_t* s = va_arg(args, segment_t*);
+    const char* sclass = va_arg(args, const char*);
+
+    if (LOG_EVENTS)
+    {
+        const auto segm_name = qpool_.acquire();
+        get_segm_name(&*segm_name, s);
+        LOG_EVENT("Segment %s class has been changed to %s", segm_name->c_str(), sclass);
     }
 }
 
