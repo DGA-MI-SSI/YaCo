05846a9ef12764322a45af7760f17fb95cec7d7b
Benoît Amiaux <benoit.amiaux@gmail.com>
2018-01-18 16:14:09 +0100

    build: add deploy_debug.cmd

---
+++ build/deploy_debug.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+set ROOT_DIR=%~dp0..
+echo %ROOT_DIR%
+
+rem plugins
+mklink yaco_plugin.py %ROOT_DIR%\YaCo\yaco_plugin.py
+
+rem yatools
+mklink /D YaTools %ROOT_DIR%\bin\yaco_d_x64\YaTools
