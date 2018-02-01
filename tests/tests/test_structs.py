d5caf1dbb7ca35db67bb953853f7c8734b4f40c3
Benoît Amiaux <benoit.amiaux@gmail.com>
2018-02-01 11:33:17 +0100

    tests: add basic struc handling tests

---
+++ tests/tests/test_structs.py
@@ -0,0 +1,42 @@
+#   Copyright (C) 2017 The YaCo Authors
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#!/bin/python
+
+import run_all_tests
+
+class Fixture(run_all_tests.Fixture):
+
+    def test_strucs(self):
+        a, b = self.setup_repos()
+        a.run(
+            self.script("idaapi.add_struc(-1, 'name_a', False)"),
+            self.save_struc("name_a"),
+        )
+        b.run(
+            self.check_struc("name_a"),
+            self.script("idaapi.set_struc_name(idaapi.get_struc_id('name_a'), 'name_b')"),
+            self.save_struc("name_a"),
+            self.save_struc("name_b"),
+        )
+        self.assertEqual(self.strucs["name_a"], "")
+        self.assertNotEqual(self.strucs["name_b"], "")
+        a.run(
+            self.check_struc("name_a"),
+            self.check_struc("name_b"),
+            self.script("idaapi.del_struc(idaapi.get_struc(idaapi.get_struc_id('name_b')))"),
+            self.save_struc("name_b"),
+        )
+        self.assertEqual(self.strucs["name_b"], "")
