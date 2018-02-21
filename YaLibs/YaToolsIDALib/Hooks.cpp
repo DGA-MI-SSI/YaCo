44f00385034cc778d0a89d130a7c09f13b14c45f
Benoît Amiaux <benoit.amiaux@gmail.com>
2018-02-21 17:24:53 +0100

    hooks: add missing math include

--- YaLibs/YaToolsIDALib/Hooks.cpp
+++ YaLibs/YaToolsIDALib/Hooks.cpp
@@ -21,6 +21,8 @@
 #include "Helpers.h"
 #include "IdaUtils.hpp"
 
+#include <math.h>
+
 // Log macro used for events logging
 #define LOG_IDP_EVENT(format, ...) do{ if(LOG_IDP_EVENTS) IDA_LOG_INFO("idp: " format, ##__VA_ARGS__); }while(0)
 #define LOG_DBG_EVENT(format, ...) do{ if(LOG_DBG_EVENTS) IDA_LOG_INFO("dbg: " format, ##__VA_ARGS__); }while(0)
