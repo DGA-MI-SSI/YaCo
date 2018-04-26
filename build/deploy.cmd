e04339246dadb0be814b65c5ed7e1f6cd21c561d
Benoît Amiaux <benoit.amiaux@gmail.com>
2018-04-26 15:30:18 +0200

    deploy: do not require administrative rights

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -4,8 +4,8 @@ set ROOT_DIR=%~dp0..
 echo %ROOT_DIR%
 
 rem plugins
-mklink yaco_plugin.py %ROOT_DIR%\YaCo\yaco_plugin.py
+mklink /H yaco_plugin.py %ROOT_DIR%\YaCo\yaco_plugin.py
 
 rem yatools
-rem mklink /D YaTools %ROOT_DIR%\bin\yaco_d_x64\YaTools
-mklink /D YaTools %ROOT_DIR%\bin\yaco_x64\YaTools
+rem mklink /J /D YaTools %ROOT_DIR%\bin\yaco_d_x64\YaTools
+mklink /J /D YaTools %ROOT_DIR%\bin\yaco_x64\YaTools
--- build/deploy_debug.cmd
+++ build/deploy_debug.cmd
@@ -4,7 +4,7 @@ set ROOT_DIR=%~dp0..
 echo %ROOT_DIR%
 
 rem plugins
-mklink yaco_plugin.py %ROOT_DIR%\YaCo\yaco_plugin.py
+mklink /H yaco_plugin.py %ROOT_DIR%\YaCo\yaco_plugin.py
 
 rem yatools
-mklink /D YaTools %ROOT_DIR%\bin\yaco_d_x64\YaTools
+mklink /J /D YaTools %ROOT_DIR%\bin\yaco_d_x64\YaTools
