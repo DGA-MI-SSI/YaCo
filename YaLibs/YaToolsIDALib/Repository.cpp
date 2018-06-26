21a194a7b37f9f2d3bb40b33cf8c7869de95d3c2
Benoît Amiaux <benoit.amiaux@gmail.com>
2018-06-26 15:40:54 +0200

    repo: enable use of standard file functions & fix compilation under msvc

--- YaLibs/YaToolsIDALib/Repository.cpp
+++ YaLibs/YaToolsIDALib/Repository.cpp
@@ -13,6 +13,7 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#define USE_STANDARD_FILE_FUNCTIONS
 #include "Ida.h"
 #include "Repository.hpp"
 
