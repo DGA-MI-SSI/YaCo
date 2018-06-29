c6ff51631e3574b38fd78855c0e53d3618321d0d
Benoît Amiaux <benoit.amiaux@gmail.com>
2018-06-29 09:50:41 +0200

    yadiff: remove merge repos function

--- YaDiff/YaDiffLib/YaDiff.hpp
+++ YaDiff/YaDiffLib/YaDiff.hpp
@@ -19,7 +19,6 @@ class YaDiff
 
     bool MergeCacheFiles(const std::string& ref_db, const std::string& new_db, const std::string& new_cache, const std::string& ref_cache);
     bool MergeCacheFiles(const std::string& ref_db, const std::string& new_db, const std::string& new_cache_db);
-    int MergeRepos(const std::string& url1, const std::string& url2, ResolveFileConflictCallback&  ResolveFileConflict);
     bool MergeDatabases(const IModel& db1, const IModel& db2, std::vector<Relation>& output);
     private:
     const Configuration& config_;
