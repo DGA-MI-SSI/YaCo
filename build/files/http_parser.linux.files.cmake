427763f70c8340333d816ba17b7ea66822859d10
Benoît Amiaux <benoit.amiaux@gmail.com>
2018-07-03 20:56:06 +0200

    build: update to libgit2 0.27.2

--- build/files/http_parser.linux.files.cmake
+++ build/files/http_parser.linux.files.cmake
@@ -1,5 +1,5 @@
 # generated with cmake
 set(_http_parser_files
-    "../deps/libgit2-0.27.0-rc1/deps/http-parser/http_parser.c"
-    "../deps/libgit2-0.27.0-rc1/deps/http-parser/http_parser.h"
+    "../deps/libgit2-0.27.2/deps/http-parser/http_parser.c"
+    "../deps/libgit2-0.27.2/deps/http-parser/http_parser.h"
 )
--- build/yadeps.cmake
+++ build/yadeps.cmake
@@ -15,7 +15,7 @@
 
 # yatools dependencies
 get_filename_component(farm_dir     "${ya_dir}/deps/farmhash-1.1"       ABSOLUTE)
-get_filename_component(git_dir      "${ya_dir}/deps/libgit2-0.27.0-rc1" ABSOLUTE)
+get_filename_component(git_dir      "${ya_dir}/deps/libgit2-0.27.2"     ABSOLUTE)
 get_filename_component(gtest_dir    "${ya_dir}/deps/gtest-1.7.0"        ABSOLUTE)
 get_filename_component(ico_dir      "${ya_dir}/deps/libiconv-1.14"      ABSOLUTE)
 get_filename_component(mbed_dir     "${ya_dir}/deps/mbedtls-2.4.2"      ABSOLUTE)
