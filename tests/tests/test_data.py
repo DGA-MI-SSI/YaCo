bc9ed59e68603cd8a8bb74a893e81cfe199e6143
Benoît Amiaux <benoit.amiaux@gmail.com>
2018-07-31 15:30:59 +0200

    tests: test extra line on unexplored data

--- tests/tests/test_data.py
+++ tests/tests/test_data.py
@@ -39,3 +39,23 @@ ida_bytes.set_cmt(ea, "some comment", 0)
 0x413b53: unexplored: unkn comm
 0x413b9c: data: data strlit ref labl name
 """)
+
+    def test_extra_line_on_unexplored_data(self):
+        a, b = self.setup_cmder()
+        a.run(
+            self.script("""
+ea = 0x413B53
+ida_lines.add_extra_line(ea, True, "some prev line")
+"""),
+            self.save_last_ea(),
+        )
+        a.check_git(added=["binary", "segment", "segment_chunk", "data"])
+
+        b.run(
+            self.check_last_ea(),
+        )
+        self.check_range(b, 0x413B40, 0x413BA4, """
+0x413b40: data: data byte ref labl
+0x413b53: unexplored: unkn line
+0x413b9c: data: data strlit ref labl name
+""")
