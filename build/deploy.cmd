12a85370f1fe3f248934022caa212a3026ea2f22
tinmarino <tinmarino@gmail.com>
2018-10-08 13:29:10 +0200

    Windows : deploy.cmd updated : need to mklink YaCo because it contains some interesting py scripts

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -7,5 +7,7 @@ rem plugins
 mklink /H yaco_plugin.py %ROOT_DIR%\YaCo\yaco_plugin.py
 
 rem yatools
-rem mklink /J /D YaTools %ROOT_DIR%\bin\yaco_d_x64\YaTools
 mklink /J /D YaTools %ROOT_DIR%\bin\yaco_x64\YaTools
+
+rem yaco
+mklink /J /D YaTools\YaCo %ROOT_DIR%\YaCo
