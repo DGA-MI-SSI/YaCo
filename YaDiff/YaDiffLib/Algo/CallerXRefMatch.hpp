a2f78d82b5cf066de3b6cc49fc501f860a670b51
tinmarino <tinmarino@gmail.com>
2019-06-04 15:49:21 +0200

    YaDiff : WarningFix : gcc complaining about uselessly defined struct (yadiff::AlogCfg)

--- YaDiff/YaDiffLib/Algo/CallerXRefMatch.hpp
+++ YaDiff/YaDiffLib/Algo/CallerXRefMatch.hpp
@@ -16,6 +16,5 @@ typedef std::map<YaToolObjectType_e, sig_map_t> sigs_container_t;
 namespace std { template<typename T> class shared_ptr; }
 
 namespace yadiff { struct IDiffAlgo; }
-struct yadiff::AlgoCfg;
 
 std::shared_ptr<yadiff::IDiffAlgo> MakeCallerXRefMatchAlgo(const yadiff::AlgoCfg& config);
