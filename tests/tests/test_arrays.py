5adba0a0d494ab2a2d37937d302c03ea19d33358
tinmarino <tinmarino@gmail.com>
2019-06-18 15:05:07 +0200

    YaTools : prettify : test array dedent to unident 3 quoted strings (otherwise hard to read)

--- tests/tests/test_arrays.py
+++ tests/tests/test_arrays.py
@@ -16,43 +16,42 @@
 #!/bin/python
 
 import runtests
+from textwrap import dedent  # Unident
 
 class Fixture(runtests.Fixture):
 
     def test_struc_arrays(self):
+        """ Test arrays in structures """
         a, b = self.setup_repos()
         a.run(
-            self.script("""
-idc.add_struc_member(idaapi.add_struc(-1, "sa", False), "fa", 0, idaapi.FF_DATA, -1, 4)
-idc.add_struc_member(idaapi.add_struc(-1, "sb", False), "fb", 0, idaapi.FF_DATA, -1, 5)
-idaapi.add_struc(-1, "sc", False)
-"""),
+            self.script(dedent("""
+                idc.add_struc_member(idaapi.add_struc(-1, "sa", False), "fa", 0, idaapi.FF_DATA, -1, 4)
+                idc.add_struc_member(idaapi.add_struc(-1, "sb", False), "fb", 0, idaapi.FF_DATA, -1, 5)
+                idaapi.add_struc(-1, "sc", False) """)),
             self.save_types(),
         )
         a.check_git(added=["struc"] * 3 + ["strucmember"] * 2)
 
         b.run(
             self.check_types(),
-            self.script("""
-sa = idc.get_struc_id("sa")
-sc = idc.get_struc_id("sc")
-sa_size = idaapi.get_struc_size(sa) * 3
-idc.add_struc_member(sc, "fa", 0, idaapi.FF_STRU | idaapi.FF_DATA, sa, sa_size, -1)
-"""),
+            self.script(dedent("""
+                sa = idc.get_struc_id("sa")
+                sc = idc.get_struc_id("sc")
+                sa_size = idaapi.get_struc_size(sa) * 3
+                idc.add_struc_member(sc, "fa", 0, idaapi.FF_STRU | idaapi.FF_DATA, sa, sa_size, -1) """),
             self.save_types(),
         )
         b.check_git(added=["strucmember"], modified=["struc"])
 
         a.run(
             self.check_types(),
-            self.script("""
-sa = idc.get_struc_id("sa")
-sb = idc.get_struc_id("sb")
-sc = idc.get_struc_id("sc")
-sa_size = idaapi.get_struc_size(sa) * 3
-sb_size = idaapi.get_struc_size(sb) * 5
-idc.add_struc_member(sc, "fb", sa_size, idaapi.FF_STRU | idaapi.FF_DATA, sb, sb_size, -1)
-"""),
+            self.script(dedent("""
+                sa = idc.get_struc_id("sa")
+                sb = idc.get_struc_id("sb")
+                sc = idc.get_struc_id("sc")
+                sa_size = idaapi.get_struc_size(sa) * 3
+                sb_size = idaapi.get_struc_size(sb) * 5
+                idc.add_struc_member(sc, "fb", sa_size, idaapi.FF_STRU | idaapi.FF_DATA, sb, sb_size, -1) """),
             self.save_types(),
         )
         a.check_git(added=["strucmember"], modified=["struc"])
@@ -62,36 +61,46 @@ idc.add_struc_member(sc, "fb", sa_size, idaapi.FF_STRU | idaapi.FF_DATA, sb, sb_
         )
 
     def test_struc_array_hole(self):
+        # Init 2 repos
         a, b = self.setup_repos()
+
+        # Test a
         a.run(
-            self.script("""
-sa = idaapi.add_struc(-1, "sa", False)
-sb = idaapi.add_struc(-1, "sb", False)
-sc = idaapi.add_struc(-1, "sc", False)
-idc.add_struc_member(sa, "field_0", 0, idaapi.FF_DATA, -1, 4)
-idc.add_struc_member(sb, "field_0", 0, idaapi.FF_DATA, -1, 6)
-sa_size = idaapi.get_struc_size(sa) * 3
-sb_size = idaapi.get_struc_size(sb) * 5
-idc.add_struc_member(sc, "field_0", 0, idaapi.FF_STRU | idaapi.FF_DATA, sa, sa_size, -1)
-idc.add_struc_member(sc, "field_C", sa_size, idaapi.FF_STRU | idaapi.FF_DATA, sb, sb_size, -1)
-"""),
+            self.script(dedent("""
+                # Create 3 structs
+                sa = idaapi.add_struc(-1, "sa", False)
+                sb = idaapi.add_struc(-1, "sb", False)
+                sc = idaapi.add_struc(-1, "sc", False)
+
+                # Add struct member, DW -> sa (must use idc wrapper)
+                ## 1. id_struct, 2. name_member, 3. offset_member, 4. flag_member, 5. type_struct
+                ## (0 for struct), 6. size_member
+                idc.add_struc_member(sa, "field_0", 0, idaapi.FF_DATA, -1, 4)
+                idc.add_struc_member(sb, "field_0", 0, idaapi.FF_DATA, -1, 6)
+                sa_size = idaapi.get_struc_size(sa) * 3
+                sb_size = idaapi.get_struc_size(sb) * 5
+
+                # Add struct member, sa -> sc
+                idc.add_struc_member(sc, "field_0", 0, idaapi.FF_STRUCT | idaapi.FF_DATA, sa, sa_size, -1)
+                idc.add_struc_member(sc, "field_C", sa_size, idaapi.FF_STRUCT | idaapi.FF_DATA, sb, sb_size, -1) """),
             self.save_types(),
         )
         a.check_git(added=["struc"] * 3 + ["strucmember"] * 4)
+
+        # Test b
         b.run(
             self.check_types(),
-            self.script("""
-sa = idc.get_struc_id("sa")
-sb = idc.get_struc_id("sb")
-sc = idc.get_struc_id("sc")
-sa_size = idaapi.get_struc_size(sa) * 2
-sb_size = idaapi.get_struc_size(sb) * 2
-idaapi.del_struc_member(idaapi.get_struc(sc), 0)
-idc.add_struc_member(sc, "field_0", 0, idaapi.FF_STRU | idaapi.FF_DATA, sa, sa_size, -1)
-fcb_offset = idaapi.get_struc_size(sa) * 3
-idaapi.del_struc_member(idaapi.get_struc(sc), fcb_offset)
-idc.add_struc_member(sc, "field_C", fcb_offset, idaapi.FF_STRU | idaapi.FF_DATA, sb, sb_size, -1)
-"""),
+            self.script(dedent("""
+                sa = idc.get_struc_id("sa")
+                sb = idc.get_struc_id("sb")
+                sc = idc.get_struc_id("sc")
+                sa_size = idaapi.get_struc_size(sa) * 2
+                sb_size = idaapi.get_struc_size(sb) * 2
+                idaapi.del_struc_member(idaapi.get_struc(sc), 0)
+                idc.add_struc_member(sc, "field_0", 0, idaapi.FF_STRU | idaapi.FF_DATA, sa, sa_size, -1)
+                fcb_offset = idaapi.get_struc_size(sa) * 3
+                idaapi.del_struc_member(idaapi.get_struc(sc), fcb_offset)
+                idc.add_struc_member(sc, "field_C", fcb_offset, idaapi.FF_STRU | idaapi.FF_DATA, sb, sb_size, -1) """)),
             self.save_types(),
         )
         b.check_git(modified=["struc"] + ["strucmember"] * 2)
