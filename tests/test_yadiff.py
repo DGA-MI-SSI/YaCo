4dc99886e78be16d971ed2e051531f191c0da2b3
tinmarino <tinmarino@gmail.com>
2019-06-18 15:04:04 +0200

    YaDiff : test : prettify : Windows lineending

--- tests/test_yadiff.py
+++ tests/test_yadiff.py
@@ -7,7 +7,7 @@ import sys
 
 def sysexec(cwd, *args):
     if True:
-        print cwd + ": " + " ".join(*args)
+        print(cwd + ": " + " ".join(*args))
     subprocess.check_call(*args, stderr=subprocess.STDOUT, shell=False)
 
 def generate_yadiff_idb(args):
@@ -19,12 +19,12 @@ def generate_yadiff_idb(args):
 def check_golden(golden_filename, got):
     expected_path = os.path.join(os.path.dirname(inspect.getsourcefile(lambda:0)), golden_filename)
 
-    # enable to update golden file
+    # Enable to update golden file
     if False:
         with open(expected_path, "wb") as fh:
             fh.write(got)
 
-    # read expected values
+    # Read expected values
     expected = None
     with open(expected_path, "rb") as fh:
         expected = fh.read()
@@ -32,8 +32,9 @@ def check_golden(golden_filename, got):
     if expected == got:
         return
 
-    # yadiff is not deterministic anymore
-    # so we want a small diff arbitrarily set at $max_lines lines
+    # Get number of line diff
+    ## yadiff is not deterministic anymore
+    ## so we want a small diff arbitrarily set at $max_lines lines
     max_lines = 100
     diff = "".join(difflib.unified_diff(expected.splitlines(1), got.splitlines(1), golden_filename, "got"))
     difflines = diff.splitlines(1)
@@ -44,7 +45,7 @@ def check_golden(golden_filename, got):
 
 def check_yadiff_database(args):
     sys.path.append(os.path.join(args.bindir, "bin"))
-    print os.path.join(args.bindir, "bin")
+    print(os.path.join(args.bindir, "bin"))
     import yadb.Root
     data = None
     with open(os.path.abspath(os.path.join(args.dst, "..", "yadiff.yadb")), "rb") as fh:
@@ -65,6 +66,8 @@ def check_yadiff_database(args):
     check_golden("test_yadiff." + sys.platform.lower() + ".golden", got)
 
 def main():
+    """ Main test for YaDiff """
+    # Get args
     parser = argparse.ArgumentParser()
     parser.add_argument("bindir", type=os.path.abspath, help="yatools binary directory")
     parser.add_argument("python", type=os.path.abspath, help="python executable")
@@ -72,7 +75,10 @@ def main():
     parser.add_argument("dst", type=os.path.abspath, help="output IDB")
     args = parser.parse_args()
 
+    # Merge idb <- YaDiff
     generate_yadiff_idb(args)
+
+    # Check
     check_yadiff_database(args)
 
 if __name__ == '__main__':
